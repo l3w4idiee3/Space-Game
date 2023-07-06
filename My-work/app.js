10330145800ec4967b129055c5f3121014dce63f
l3w4idiee3 <lewisjassy43@gmail.com>
2023-07-06 09:44:44 +0300

    introduced canvas

--- My-work/app.js
+++ My-work/app.js
@@ -1,2 +1,5 @@
 canvas = document.getElementById('myCanvas');
 ctx = canvas.getContext('2d');
+ctx.fillStyle = 'black';
+// set the fill color to black
+ctx.fillRect(0,0, canvas.height, canvas.width)
