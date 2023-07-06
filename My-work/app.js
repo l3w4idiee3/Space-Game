57663bd1f6f60e1b8fb5e6f9c57a66045c6b8f4a
l3w4idiee3 <lewisjassy43@gmail.com>
2023-07-06 10:07:40 +0300

    added a loadasset function

--- My-work/app.js
+++ My-work/app.js
@@ -3,3 +3,13 @@ ctx = canvas.getContext('2d');
 ctx.fillStyle = 'black';
 // set the fill color to black
 ctx.fillRect(0,0, canvas.height, canvas.width)
+
+function loadAsset(path) {
+    return new Promise((resolve) => {
+        const img = new Image();
+        img.src = path;
+        img.onload = () => {
+            resolve(img);
+        }
+    })
+}
